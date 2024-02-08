d7a787daae17e2bf045eb1cc78a14780c5c5d1d1
Tanukichi-35 <yo-fu@outlook.jp>
2024-02-09 05:12:22 +0900

    ログインユーザー名を表示するよう変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,8 +6,8 @@
 
 @section('link')
 <ul>
-  <li><a href="">ホーム</a></li>
-  <li><a href="">日付一覧</a></li>
+  <li><a href="/">ホーム</a></li>
+  <li><a href="/attendance">日付一覧</a></li>
   <li>
     <form action="/logout" method="POST">
       @csrf
@@ -19,17 +19,17 @@
 
 @section('content')
 <div class="div__main">
-  <h2 class="h2__atte">〇〇さんお疲れ様です！</h2>
+  <h2 class="h2__atte">{{"$user->name"}}さんお疲れ様です！</h2>
   <form method="POST" class="form__atte">
     @csrf
     <div class="div__menu">
       <div class="div__menu-upper">
         <button formaction="/work-start" class="button__work-start">勤務開始</button>
-        <button formaction="/work-end" class="button__work-end">勤務終了</button>
+        <button formaction="/work-end" class="button__work-end" disabled>勤務終了</button>
       </div>
       <div class="div__menu-lower">
-        <button formaction="/break-start" class="button__break-start">休憩開始</button>
-        <button formaction="/break-end" class="button__break-end">休憩終了</button>
+        <button formaction="/break-start" class="button__break-start" disabled>休憩開始</button>
+        <button formaction="/break-end" class="button__break-end" disabled>休憩終了</button>
       </div>
     </div>
   </form>
